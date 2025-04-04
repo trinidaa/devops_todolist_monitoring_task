3f180bfa145c797dbfd076f1e9114729dff3481c
Viacheslav Kaliak <trinidaa@gmial.com>
2025-04-04 15:42:24 +0200

    Solution

--- src/todolist/settings.py
+++ src/todolist/settings.py
@@ -1,13 +1,3 @@
-"""
-Django settings for todolist project.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.7/topics/settings/
-
-For the full list of settings and their values, see
-https://docs.djangoproject.com/en/1.7/ref/settings/
-"""
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 
